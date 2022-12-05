0cd4eed1436132ca9953b2eb6fe48e0388e435b5
rolando <jakob.b.work2@gmail.com>
2022-12-05 20:52:24 +0100

    Solve task 1 day 5

---
+++ day5/solution.py
@@ -0,0 +1,37 @@
+rawdata = ""
+with open('./actualdata.txt', encoding="utf-8") as inputfile:
+  rawdata = inputfile.read()
+
+[cratesStr, instructions] = rawdata.split('\n\n')
+print(cratesStr)
+
+rows = cratesStr.split('\n')
+cols = rows[-1].split()
+print(cols)
+stacks = {}
+
+# Setup
+rows.reverse()
+rows = rows[1:]
+for row in rows:
+  for col in cols:
+    startIndex = 1 + (int(col) - 1) * 4
+    cargo = row[startIndex: startIndex+1]
+    if (cargo != ' '):
+      if (type(stacks.get(col)) is not list):
+        stacks[col] = [cargo]
+      else:
+        stacks[col].append(cargo)
+
+# Compute
+for instruction in instructions.splitlines():
+  splittedInstruction = instruction.split()
+  iterations = int(splittedInstruction[1])
+  fromColumn = splittedInstruction[3]
+  toColumn = splittedInstruction[5]
+  for i in range(0, iterations):
+    stacks[toColumn].append(stacks[fromColumn].pop())
+
+for col in cols:
+  print(stacks[col][-1])
+
